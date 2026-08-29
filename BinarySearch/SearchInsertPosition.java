b7eaf1fba864c49d567f234661942d208c4bd51e
saikiran-sheripalli <sheripallisaikiran@gmail.com>
2026-08-29 06:57:28 +0530

    completed Search Insert Position

--- BinarySearch/SearchInsertPosition.java
+++ BinarySearch/SearchInsertPosition.java
@@ -1,25 +1,27 @@
 public class SearchInsertPosition {
     public static void main(String[] args) {
-        int[] nums = {1, 3, 5, 6};
-        int target = 4;
+        int[] nums = {1, 3, 5, 6, 10};
+        int target = 11;
+
+        System.out.println(searchInsertionPosition(nums, target));
 
     }
 
     static int searchInsertionPosition(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-
-        while(start < end) {
-            int mid = start + (end - start) / 2;
+        int mid = 0;
+        while(start <= end) {
+            mid = start + (end - start) / 2;
 
             if (target == nums[mid]) {
                 return mid;
             } else if (target > nums[mid]) {
                 start = mid + 1;
             } else if (target < nums[mid]) {
-
+                end = mid - 1;
             }
         }
-        return -1;
+        return start;
     }
 }
