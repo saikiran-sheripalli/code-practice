daadc807a537ddd193adc39c339ae5593b04311e
saikiran-sheripalli <sheripallisaikiran@gmail.com>
2026-09-01 16:32:11 +0530

    corrected core logic of FindFirstAndLastPosition

--- BinarySearch/tier2/FindFirstandLastPositionofElementInSortedArray.java
+++ BinarySearch/tier2/FindFirstandLastPositionofElementInSortedArray.java
@@ -6,33 +6,38 @@ public class FindFirstandLastPositionofElementInSortedArray {
     static int[] arr;
     static int target;
 
-    static int[] findFirstAndLastOccurenceOfaTarget(int[] nums, int target) {
+    static int findTheBoundaryPosition(int[] nums, int target, boolean firstBound) {
         int start = 0;
         int end = nums.length - 1;
+        int result = -1;
 
-        while (start < end) {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
             if (arr[mid] == target) {
-                if (arr[mid + 1] == target) {
-                    return new int[]{mid, mid + 1};
+                result = mid;
+                if (firstBound) {
+                    end = mid -1;
+                } else {
+                    start = mid +1;
                 }
-                if (arr[mid - 1] == target) {
-                    return new int[]{mid - 1, mid};
-                }
+
             } else if (arr[mid] > target) {
                 end = mid - 1;
             } else if (arr[mid] < target) {
                 start = mid + 1;
             }
         }
-        return new int[]{-1,-1};
+        return result;
     }
 
     public static void main(String[] args) {
 
         arr = new int[]{5, 7, 7, 8, 8, 10};
-        target = 6;
-        System.out.println(Arrays.toString(findFirstAndLastOccurenceOfaTarget(arr, target)));
-
+        target = 8;
+        int[] result = new int[] {-1,-1};
+        int firstBound = findTheBoundaryPosition(arr, target, true);
+        if (firstBound < 0) System.out.println(result);
+        int lastBound = findTheBoundaryPosition(arr, target, false);
+        System.out.println(Arrays.toString(new int[] {firstBound,lastBound}));
     }
 }
